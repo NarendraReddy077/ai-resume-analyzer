d35cab5533c1e7e2a2c6b5a7581ef9a7af490e57
NarendraReddy077 <narendrareddymolakala@gmail.com>
2025-12-18 18:17:05 +0530

    Updated navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
             <Link to='/'>
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                <p className="text-lg max-sm:text-base md:text-2xl font-bold text-gradient">RESUMIND</p>
             </Link>
-            <Link to='/upload' className="primary-button w-fit">
+            <Link to='/upload' className="primary-button w-fit text-sm max-sm:text-xs md:text-base px-3 max-sm:px-2 py-2 max-sm:py-1">
                 Upload Resume
             </Link>
         </nav>
